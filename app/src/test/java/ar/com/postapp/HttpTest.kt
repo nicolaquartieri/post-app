fc867f9fe71a5a4540e157d57a1007f168ab4677
nicolaquartieri <nicolas.quartieri@gmail.com>
2021-04-29 19:05:18 +0100

    Unit test the corouting handler error over the HTTP/S

--- app/src/test/java/ar/com/postapp/HttpTest.kt
+++ app/src/test/java/ar/com/postapp/HttpTest.kt
@@ -1,9 +1,13 @@
 package ar.com.postapp
 
+import ar.com.infrastructure.repositories.utils.Http.httpError
 import ar.com.infrastructure.repositories.utils.InfrastructureException
 import ar.com.infrastructure.repositories.utils.infrastructureExceptionHandler
-import kotlinx.coroutines.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
+import org.junit.Assert.*
 import org.junit.Test
 
 @ExperimentalCoroutinesApi
@@ -12,8 +16,19 @@ class HttpTest {
     @Test
     fun should_return_httpError() = runBlockingTest {
         launch(retry) {
-            getError()
+            val value = doSomething()
+            assertEquals("Any value", value)
         }
+    }
+
+    private suspend fun doSomething(): String {
+        return getValue(false)
+    }
+
+    private suspend fun getValue(state: Boolean): String = httpError {
+        if (state) "Any value"
+        else throw InfrastructureException(Exception("Something happen in the network"))
+
     }
 
     private fun getError(): InfrastructureException {
